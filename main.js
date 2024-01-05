290bfc8299b8a5377771b10db8d121bffb81c631
sana <135113898+tasonaso@users.noreply.github.com>
2024-01-05 15:51:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ function loadImage(obj) {
     for (i = 0; i < obj.files.length; i++) {
         var fileReader = new FileReader();
         fileReader.onload = (function (e) {
+            console.log(e);
+            console.log(e.target);
+            console.log(e.target.result);
             var amarker = document.getElementById("pattmarker");
             amarker.setAttribute("url", e.target.result);
             key++;
