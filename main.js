ff06f312a3255161cfdb576e9a10b1219f4739bb
sana <135113898+tasonaso@users.noreply.github.com>
2024-01-05 16:39:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 let key = 0;
-function loadImage(obj) {
-    for (i = 0; i < obj.files.length; i++) {
-        var fileReader = new FileReader();
-        fileReader.onload = (function (e) {
-            var objectURL = URL.createObjectURL(e.target.result);
-            var amarker = document.getElementById("pattmarker");
-            
-            console.log(objectURL);
-            amarker.setAttribute("url", objectURL);
-            key++;
-        });
-        fileReader.readAsDataURL(obj.files[i]);
-    }
+function loadImage(e) {
+    // アップロードしたファイルのURLを取得
+    var objectURL = URL.createObjectURL(e.target.files[0]);
+    var amarker = document.getElementById("pattmarker");
+    console.log(objectURL);
+    amarker.setAttribute("url", objectURL);
 }
