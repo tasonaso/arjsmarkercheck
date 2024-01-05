47ebdbf1c800bac0b5bd343f4ca1b6dfa479be1e
sana <135113898+tasonaso@users.noreply.github.com>
2024-01-05 16:41:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let key = 0;
 function loadImage(e) {
     // アップロードしたファイルのURLを取得
-    var objectURL = URL.createObjectURL(e.target.files[0]);
+    var objectURL = URL.createObjectURL(e.files[0]);
     var amarker = document.getElementById("pattmarker");
     console.log(objectURL);
     amarker.setAttribute("url", objectURL);
