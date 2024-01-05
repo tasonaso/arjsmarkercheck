1a94a3e60653d92116d0ceb38848da8d4cf54c83
sana <135113898+tasonaso@users.noreply.github.com>
2024-01-05 17:04:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let key = 0;
 function loadImage(e) {
     // アップロードしたファイルのURLを取得
+    if(e == null) return;
     var objectURL = URL.createObjectURL(e.files[0]);
     var amarker = document.getElementById("arwindow").contentWindow.document.getElementById("pattmarker");
     console.log("objectURL = "+objectURL);
