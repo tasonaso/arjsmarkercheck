642e192c13c56809668c59b1d3c8582f47aa901f
sana <135113898+tasonaso@users.noreply.github.com>
2024-01-07 22:57:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ function loadImage(e) {
     // アップロードしたファイルのURLを取得
     if(e == null) return;
     var objectURL = URL.createObjectURL(e.files[0]);
-    var amarker = document.getElementById("arwindow").contentWindow.document.getElementById("pattmarker");
+    // var amarker = document.getElementById("arwindow").contentWindow.document.getElementById("pattmarker");
+    var amarker = document.getElementById("pattmarker")
     console.log("objectURL = "+objectURL);
     amarker.setAttribute("url", "pattern.ptt");
 }
